0555d130d3bb37f43962f37a5b139f4d2a9fa6b0
zaratustrat <44696350+zaratustrat@users.noreply.github.com>
2019-02-16 08:33:03 +0200

    slider

--- cardboard/js/script.js
+++ cardboard/js/script.js
@@ -10,4 +10,45 @@ $(document).ready(function () {
  link_active.click(function () {
   link.removeClass('menu-link_active');
  });
+}); 
+
+$('.slider').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ arrows: false,
+ fade: true,
+ asNavFor: '.slider__item'
+});
+$('.slider__item').slick({
+ slidesToShow: 3,
+ slidesToScroll: 1,
+ asNavFor: '.slider',
+ dots: true,
+ centerMode: true,
+ focusOnSelect: true,
+ nextArrow: '<div class="arrow arrow_right"></div>',
+ prevArrow: '<div class="arrow arrow_left"></div>',
+ responsive: [
+  {
+   breakpoint: 1200,
+   settings: {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+   }
+  },
+  {
+   breakpoint: 1024,
+   settings: {
+    slidesToShow: 2,
+    slidesToScroll: 1
+   }
+  },
+  {
+   breakpoint: 768,
+   settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1
+   }
+  }
+ ]
 });
