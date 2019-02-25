c3d1a23c57afc9347a9d8148d2bee3e0fedbd2ef
zaratustrat <44696350+zaratustrat@users.noreply.github.com>
2019-02-25 19:01:00 +0200

    modal

--- cardboard/js/script.js
+++ cardboard/js/script.js
@@ -94,4 +94,15 @@ $('.reviews-slider').slick({
    }
   }
  ]
-});
+}); 
+
+$(document).ready(function () {
+ $('#button-callback').on('click', function () {
+  $('#modal-callback').toggleClass('modal_visible');
+ });
+ $('.modal-close').on('click', function () {
+  $('#modal-callback').toggleClass('modal_visible');
+ });
+}); 
+
+
